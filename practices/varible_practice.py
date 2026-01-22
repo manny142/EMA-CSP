7ddb30489f1eb936a36f7f4f23f44383d4b0a4bb
manny142 <erick.alvidrez@ucas-edu.net>
2026-01-22 11:42:36 -0700

    varidles pracice

---
+++ practices/varible_practice.py
@@ -0,0 +1,14 @@
+# EA, Variable practice
+
+name = "Erick"
+numder = 4
+numder2 = 300
+breakfast = "pancks"
+color = "black"
+school = "ucas"
+year = "2026"
+eye = "brown"
+age = "14"
+sudject = "Cps"
+print(name , numder , numder2 , breakfast , color , school , year , eye , age , sudject)
+
