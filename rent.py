604cf74f38e94a98afc5950cddad75f15288880a
manny142 <erick.alvidrez@ucas-edu.net>
2026-01-23 11:43:59 -0700

    floats

---
+++ rent.py
@@ -0,0 +1,7 @@
+money = float(input("how much do you earn in a month: $"))
+rent = float(input("how much is ypure rent?: $"))
+utilities = float(input("how ,much do you spend on utilities?: $"))
+groceries = float(input("how much is do you spend on groceries: $"))
+transportation = float(input("how much do you spend on transportion?: $"))
+
+print(money - rent + utilities + groceries + transportation)
