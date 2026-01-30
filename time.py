c27ed4a1e1307f5932d46a9befb972c21d960ea0
manny142 <erick.alvidrez@ucas-edu.net>
2026-01-30 11:49:53 -0700

    time

---
+++ time.py
@@ -0,0 +1,7 @@
+
+time = int(input("what time it it in mititary time: (example 1200)"))
+
+if time >= 100 and time < 1159:
+    print("good morning")
+elif time >= 1200 and time < 1700:
+    print("good afternoon")
