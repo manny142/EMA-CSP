915b96397c35e8a9ba56aa4be6b697b68d23a1b8
manny142 <erick.alvidrez@ucas-edu.net>
2026-02-05 11:49:23 -0700

    UPDATE

--- rent.py
+++ rent.py
@@ -4,9 +4,12 @@ utilities = float(input("how ,much do you spend on utilities?: $"))
 groceries = float(input("how much is do you spend on groceries: $"))
 transportation = float(input("how much do you spend on transportion?: $"))
 
-print("youre rent is 1200.00 and that is 40% of you income")    
-print("Your utilities are $ 200.00 and that is 7 % of your income.")
-print("Your groceries are $ 250.00 and that is 8 % of your income.")                    
-print( "Your transportation is $ 500.00 and that is 17 % of your income.")    
-print("You should save $ 300.00 a month, that is 10 % of your income.") 
-print("You have $ 550.00 of spending money each month! ")   
+save = money*0.10
+left = money - rent - utilities - groceries - transportation
+remt = rent/money*100
+print(f"youre rent is ${rent} and that is {remt}% of you income")    
+print(f"Your utilities are ${utilities} and that is 7 % of your income.")
+print(f"Your groceries are ${groceries} and that is 8 % of your income.")                    
+print(f"Your transportation is ${transportation} and that is 17 % of your income.")    
+print(f"You should save ${save} a month, that is 10 % of your income.") 
+print(f"You have {left}% of spending money each month! ")   
