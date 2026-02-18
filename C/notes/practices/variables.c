8d9c5b51890292e9c4ec589011b144f3793fca60
manny142 <erick.alvidrez@ucas-edu.net>
2026-02-18 11:49:28 -0700

    C

---
+++ C/notes/practices/variables.c
@@ -0,0 +1,11 @@
+// comment
+#include <stdio.h> // lets put inputs and outputs
+
+int main(){
+    int number = 12; // every time i end a stament i need ;
+    float pi = 3.14;
+
+    printf("%d" , number);
+    printf("%f\n" , pi);// \n tells computer that its a new line
+    return 0; // always the last line of main
+}
