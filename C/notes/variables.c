f9b811f52a8ae790e8de271cdfad1aec2a2e05c6
manny142 <erick.alvidrez@ucas-edu.net>
2026-02-19 11:49:24 -0700

    working

--- C/notes/variables.c
+++ C/notes/variables.c
@@ -4,8 +4,18 @@
 int main(){
     int number = 12; // every time i end a stament i need ;
     float pi = 3.14;
+    char name[] = "Xavier";
+    char person[50];
+    int age;
+   
+    printf("tell me youre name\n");
+    scanf("%d", &age);
 
-    printf("%d" , number);
+    printf("tell me how old you are\n");
+    scanf("%d", &age);
+
+    printf("%d\n" , number);
     printf("%f\n" , pi);// \n tells computer that its a new line
+    printf("%s is %d years old\n", name , age);
     return 0; // always the last line of main
 }
---
+++ C/practices/firstpractice.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main(){
+    
+    char person[50];
+    int number;
+    int age;
+
+    printf("tell me youre name\n");
+    scanf("%s", person);
+
+    printf("what is you faviorte numeder\n");
+    scanf("%d", number);
+
+    printf("name: %s", person);
+
+    return 0;
+}
+
