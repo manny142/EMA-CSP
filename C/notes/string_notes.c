1d604b1d4e75548f7022c7388eead4dd9dd42159
manny142 <erick.alvidrez@ucas-edu.net>
2026-02-24 11:49:59 -0700

    Add initial Python script with basic output

---
+++ C/notes/string_notes.c
@@ -0,0 +1,14 @@
+// EA sting notes
+#include <stdio.h>
+
+int main(){
+    const char* wing = " _________";
+    const char* topcar = " _|_____|_";
+    const char* car = "| oo____oo |";
+   
+    printf("%s\n", wing);
+    printf("%s\n", topcar);
+    printf("%s\n", car);
+    
+    return 0;
+}
