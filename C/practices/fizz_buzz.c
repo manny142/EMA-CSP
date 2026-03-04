26e954d7852418d6ec40cbd721730a0e8084d784
manny142 <erick.alvidrez@ucas-edu.net>
2026-03-04 11:26:18 -0700

    Implement code changes to enhance functionality and improve performance

---
+++ C/practices/fizz_buzz.c
@@ -0,0 +1,22 @@
+//ea,3, fizzbuzz
+
+#include <stdio.h>
+
+int main(){
+    int sum = 0;
+int i;
+
+for (i = 1; i <= 50; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        printf("FizzBuzz\n");
+    } else if (i % 3 == 0) {
+        printf("Fizz\n");
+    } else if (i % 5 == 0) {
+        printf("Buzz\n");
+    } else {
+        printf("%d\n", i);
+    }
+}
+
+    return 0;
+}
